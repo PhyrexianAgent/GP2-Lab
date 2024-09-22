1386df1dc63ce41cb723fe8df91cf95d899a2f0e
PhyrexianAgent <aidanthewizard5@gmail.com>
2024-09-21 21:08:41 -0400

    Implemented a Minimum Dot
    Allows control of how strait forward the check for the player is. When 1 the dot will basically wait for the player to be in a direct line in front of it, when 0 will check ahead of the sides. Hope this explains it a bit, maybe you would understand better looking how I did it.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,6 +8,7 @@ public class EnemyController : MonoBehaviour
     [SerializeField] private Transform[] patrolPoints;
     [SerializeField] private float lookTime = 1f;
     [SerializeField] private float initialLookTime = 3f;
+    [SerializeField, Range(0, 1)] private float minimumDot = 0.1f;
 
     private UnityEngine.AI.NavMeshAgent nav;
 
@@ -16,18 +17,22 @@ public class EnemyController : MonoBehaviour
         nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
+    float MinValue(float value, float min){
+        if (value > 0 && value < min)
+            value = -1;
+        return value;
+    }
+
     bool CanSeePlayer(){
         Vector3 diff = player.position - transform.position;
         diff.Normalize();
-        return Vector3.Dot(transform.forward, diff) > 0;
+        //Debug.Log(Vector3.Dot(transform.forward, diff));
+        return MinValue(Vector3.Dot(transform.forward, diff), minimumDot) > 0;
     }
     void Update()
     {
         if (CanSeePlayer()){
             Debug.Log("Can See Player");
-        }
-        else{
-            Debug.Log("Cant See Player");
         }
     }
 }
